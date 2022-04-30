926fb88fff92d0dedd91e308d216f91020aebec0
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-04-30 17:26:29 -0300

    Create Not Found page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
+import AuthProvider from './AuthProvider';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<p>PRODE HOME</p>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<p>PRODE HOME</p>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
