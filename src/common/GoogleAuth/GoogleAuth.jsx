95961b88a97a74c36cdbe9a99ea59e1827f0f79e
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-03 19:08:37 -0300

    Remove setcookie

--- src/common/GoogleAuth/GoogleAuth.jsx
+++ src/common/GoogleAuth/GoogleAuth.jsx
@@ -6,15 +6,15 @@ import config from '../../Constants';
 import Container, { AuthLink } from './GoogleAuth.styles';
 
 function GoogleAuth() {
-  function setCookie(name, value, days) {
-    let expires = '';
-    if (days) {
-      const date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = `; expires=${date.toUTCString()}`;
-    }
-    document.cookie = `${name}=${value || ''}${expires}; path=/`;
-  }
+  // function setCookie(name, value, days) {
+  //   let expires = '';
+  //   if (days) {
+  //     const date = new Date();
+  //     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  //     expires = `; expires=${date.toUTCString()}`;
+  //   }
+  //   document.cookie = `${name}=${value || ''}${expires}; path=/`;
+  // }
   const responseGoogle = async (response) => {
     const res = await axios.post(
       `${config.API_URL}/auth/google`,
@@ -25,9 +25,10 @@ function GoogleAuth() {
         withCredentials: true,
       }
     );
-    if (res.status === 200) {
-      setCookie('jwt', response.tokenId, 30);
-    }
+    console.log(res);
+    // if (res.status === 200) {
+    //   setCookie('jwt', response.tokenId, 30);
+    // }
   };
 
   return (
