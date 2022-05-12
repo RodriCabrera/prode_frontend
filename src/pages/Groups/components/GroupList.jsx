3ed217482db7569dd50179cb7a5d7664793ba6b0
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-12 12:04:54 -0300

    links a la pagina de cada grupo

--- src/pages/Groups/components/GroupList.jsx
+++ src/pages/Groups/components/GroupList.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function GroupList({ groups }) {
   return (
     <>
       {groups.length === 0 && <p>No perteneces a ningún grupo</p>}
       <ul>
-        {groups.length > 0 && groups.map((group) => <li> {group.name}</li>)}
+        {groups.length > 0 &&
+          groups.map((group) => (
+            <li>
+              <Link to={`/groups/${group.name}`}>{group.name}</Link>
+            </li>
+          ))}
       </ul>
     </>
   );
