0e80cbd90130b84e6e78408d00a0176ff48c33dc
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-12 20:34:25 -0300

    Spinner & Loading screen cambiar spinner por uno horizontal. que se adapte mas facil agregada pagina Loading, para poner a pantalla completa cuando este cargando

--- src/common/Spinner/Spinner.jsx
+++ src/common/Spinner/Spinner.jsx
@@ -1,36 +1,6 @@
-import styled from '@emotion/styled';
 import React from 'react';
 import './spinner.css';
 
-// TODO Reemplazar por uno que se adapte mejor.
-
-const SpinnerWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  /* height: 100vh; */
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-`;
-
 export function Spinner() {
-  return (
-    <SpinnerWrapper>
-      <div className="lds-default">
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
-    </SpinnerWrapper>
-  );
+  return <div className="line-wobble" />;
 }
