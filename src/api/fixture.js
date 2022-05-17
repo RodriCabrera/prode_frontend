e9625ed8da271afbe896a3e6e9ab56848883ac32
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-17 16:45:19 -0300

    fixture con instancias de axios

--- src/api/fixture.js
+++ src/api/fixture.js
@@ -1,13 +1,11 @@
-import axios from 'axios';
-import config from '../Constants';
+import { withoutCredentials } from './instances';
 
 export const getFixture = (groupId, stageId) => {
-  return axios.get(
-    `${config.API_URL}
-    /fifa/fixture?groupId=${groupId || ''}&stageId=${stageId || ''}`
+  return withoutCredentials.get(
+    `/fifa/fixture?groupId=${groupId || ''}&stageId=${stageId || ''}`
   );
 };
 
 export const getGroupStage = () => {
-  return axios.get(`${config.API_URL}/fifa/groups`);
+  return withoutCredentials.get('/fifa/groups');
 };
