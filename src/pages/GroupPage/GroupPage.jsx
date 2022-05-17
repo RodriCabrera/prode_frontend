bc400f7681b56159bd5002510b2b76f1a6987b76
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-05-17 19:44:23 -0300

    group page btn y toast

--- src/pages/GroupPage/GroupPage.jsx
+++ src/pages/GroupPage/GroupPage.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { HiOutlineUserGroup } from 'react-icons/hi';
 import { isEmpty } from 'lodash';
-import { getGroupData, getGroupScores } from '../../api/groups';
+import { toast } from 'react-toastify';
+import { getGroupData, getGroupScores, leaveGroup } from '../../api/groups';
 import ListWrapper from '../../common/Lists/ListWrapper';
 import { Spinner } from '../../common/Spinner/Spinner';
+import { Text, Button } from '../../common/common.styles';
 
 // TODO: Boton para salir del grupo.
 // TODO: Mostrar acuerdo/reglas del grupo.
@@ -16,24 +18,36 @@ function GroupPage() {
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // TODO: Implementar toast promise:
-    getGroupData(name)
-      .then(({ data }) => {
-        setGroup(data.groupData);
-      })
-      .catch((error) => {
-        // TODO: handle error : Mostrar el msg en el toast?
-        if (error.request.status === 401) {
-          alert('You are not a member of this group');
-        } else if (error.request.status === 400) {
-          alert('Group does not exist');
-        } else {
-          console.error(error);
-        }
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    // // TODO: Implementar toast promise:
+    // ? Queda como toast o mejor display en el body que diga si el grupo no existe o el usuario no es parte?
+    toast.promise(
+      getGroupData(name)
+        .then(({ data }) => {
+          setGroup(data.groupData);
+        })
+        //   // TODO: handle error : Mostrar el msg en el toast?
+        // .catch((error) => {
+        //   if (error.request.status === 401) {
+        //     alert('You are not a member of this group');
+        //   } else if (error.request.status === 400) {
+        //     alert('Group does not exist');
+        //   } else {
+        //     console.error(error);
+        //   }
+        // })
+        .finally(() => {
+          setIsLoading(false);
+        }),
+      {
+        pending: 'Loading group data...',
+        success: 'Group data loaded',
+        error: {
+          render({ data }) {
+            return data.response.data.error;
+          },
+        },
+      }
+    );
   }, []);
 
   useEffect(() => {
@@ -47,14 +61,28 @@ function GroupPage() {
       });
   }, []);
 
+  const handleLeaveGroup = () => {
+    toast.promise(leaveGroup(name), {
+      pending: 'Leaving group...',
+      success: 'You left the group',
+      error: {
+        render({ data }) {
+          return data.response.data.error;
+        },
+      },
+    });
+  };
+
   return (
     <>
       {isLoading && <Spinner />}
       {group.name && (
         <>
-          <h1>{group.name}</h1>
-          <h2>Admin: {group.owner.name}</h2>
-          <h3>Members:</h3>
+          <Text size="2.5rem" align="center">
+            {group.name}
+          </Text>
+          <Text size="1.5rem">Admin: {group.owner.name}</Text>
+          <Text size="1.5rem">Members:</Text>
           {/* <ul>
             {group.members.map((member) => (
               <li key={member._id}>{member.name}</li>
@@ -67,9 +95,10 @@ function GroupPage() {
                   key={score.user}
                   avatar={<HiOutlineUserGroup size="1.8rem" />}
                 >
-                  <p>{`${score.user} : ${score.score} pts`}</p>
+                  <Text>{`${score.user} : ${score.score} pts`}</Text>
                 </ListWrapper>
               ))}
+          <Button onClick={handleLeaveGroup}>Leave group</Button>
         </>
       )}
     </>
