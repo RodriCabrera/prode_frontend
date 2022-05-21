cf3e49f6b178b997debd466079bd4e4b141bf765
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-20 22:27:15 -0300

    remove unused imports

--- src/pages/Predictions/Predictions.jsx
+++ src/pages/Predictions/Predictions.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Navigate, Outlet, useNavigate } from 'react-router-dom';
-import { Button, Text } from '../../common/common.styles';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Text } from '../../common/common.styles';
 import ToggleSwitch from '../../common/ToggleSwitch';
 import {
   BannerTitle,
-  OptionsWrapper,
   PredictionsPageWrapper,
   ToggleContainer,
   ToggleWrapper,
@@ -33,19 +32,6 @@ function Predictions() {
           </Text>
         </ToggleWrapper>
       </ToggleContainer>
-      {/* {mode === 'resultados' ? (
-        <Navigate to="/predictions/results" />
-      ) : (
-        <Navigate to="/predictions/edit" />
-      )} */}
-      {/* <OptionsWrapper>
-        <Button grayscale>
-          <Link to="/predictions/results">RESULTADOS</Link>
-        </Button>
-        <Button grayscale>
-          <Link to="/predictions/edit">PREDECIR</Link>
-        </Button>
-      </OptionsWrapper> */}
       <Outlet />
     </PredictionsPageWrapper>
   );
