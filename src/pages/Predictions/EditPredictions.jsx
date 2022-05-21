dfeaac645f963e60b711831176e28ec0bd817d00
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-21 19:47:38 -0300

    selector de grupos para predecir

--- src/pages/Predictions/EditPredictions.jsx
+++ src/pages/Predictions/EditPredictions.jsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { getUserGroups } from '../../api/groups';
+import { Text } from '../../common/common.styles';
+import ListElement from '../../common/Lists/ListElement';
+import { Spinner } from '../../common/Spinner/Spinner';
+
+const GroupsListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
 function EditPredictions() {
-  return <Outlet />;
+  const [groupList, setGroupList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedGroup, setselectedGroup] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getUserGroups()
+      .then(({ data }) => {
+        setGroupList(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleGroupSelect = (groupId) => {
+    console.log(groupId);
+  };
+
+  console.log('groupList', groupList);
+  return (
+    <>
+      <Text>Para que grupo son estas predicciones??</Text>
+      <GroupsListWrapper>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          groupList?.map((group) => {
+            return (
+              <ListElement onClick={() => handleGroupSelect(group.id)}>
+                {group.name}
+              </ListElement>
+            );
+          })
+        )}
+      </GroupsListWrapper>
+      <Outlet context={[selectedGroup]} />
+    </>
+  );
 }
 
 export default EditPredictions;
