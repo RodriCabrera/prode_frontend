b0266f3418621400dcccada7a25f3a3371fa15e9
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-27 17:54:59 -0300

    remove pts from banners

--- src/pages/Predictions/BannerList.jsx
+++ src/pages/Predictions/BannerList.jsx
@@ -15,35 +15,30 @@ function BannerList() {
           <BannerTitle>GRUPOS</BannerTitle>
           <p>Ver/Editar</p>
         </div>
-        <p>pts</p>
       </Banner>
       <Banner onClick={() => navigate('later-stages/16round')}>
         <div>
           <BannerTitle>OCTAVOS</BannerTitle>
           <p>Ver/Editar</p>
         </div>
-        <p>pts</p>
       </Banner>
       <Banner onClick={() => navigate('later-stages/8round')}>
         <div>
           <BannerTitle>CUARTOS</BannerTitle>
           <p>Ver/Editar</p>
         </div>
-        <p>pts</p>
       </Banner>
       <Banner onClick={() => navigate('later-stages/semis')}>
         <div>
           <BannerTitle>SEMIFINALES</BannerTitle>
           <p>Ver/Editar</p>
         </div>
-        <p>pts</p>
       </Banner>
       <Banner onClick={() => navigate('later-stages/final')}>
         <div>
           <BannerTitle>FINAL</BannerTitle>
           <p>Ver/Editar</p>
         </div>
-        <p>pts</p>
       </Banner>
     </BannerContainer>
   );
