4f86aca94bbed8180c91458165843b526c49324d
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-05-27 21:35:43 -0300

    LinkBar styles

--- src/common/Linkbar/Linkbar.jsx
+++ src/common/Linkbar/Linkbar.jsx
@@ -1,27 +1,61 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ButtonGroup, NavbarWrapper } from '../Navbar/Navbar.styles';
+
+const LinkbarContainer = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+const LinkbarWrapper = styled(NavbarWrapper)`
+  width: 100%;
+  max-width: 700px;
+  justify-content: center;
+`;
 
 const LinkGroup = styled(ButtonGroup)`
   justify-content: space-around;
   align-items: center;
-  max-width: 50%;
+  padding: 0;
 `;
 
-const LinkbarWrapper = styled(NavbarWrapper)`
-  justify-content: center;
+const CustomLink = styled(Link)`
+  text-decoration: none;
+  text-align: center;
+  font-weight: ${({ isCurrent }) => isCurrent && '800'};
+  font-size: ${({ isCurrent }) => isCurrent && '1.2rem'};
 `;
 
 export function Linkbar() {
+  const location = useLocation();
+  const basePath = location.pathname.split('/')[1];
+
+  const isCurrent = (link) => {
+    return link === basePath;
+  };
+  console.log('iscurrent fixture', isCurrent('fixture'));
   return (
-    <LinkbarWrapper>
-      <LinkGroup id="button-group-left">
-        <Link to="/fixture">Fixture</Link>
-        <Link to="/predictions/edit">Mis Predicciones</Link>
-        <Link to="/scores">Tabla de Puntajes</Link>
-        <Link to="/groups">Gestionar grupos</Link>
-      </LinkGroup>
-    </LinkbarWrapper>
+    <LinkbarContainer>
+      <LinkbarWrapper>
+        <LinkGroup id="button-group-left">
+          <CustomLink to="/fixture" isCurrent={isCurrent('fixture')}>
+            Fixture
+          </CustomLink>
+          <CustomLink
+            to="/predictions/edit"
+            isCurrent={isCurrent('predictions')}
+          >
+            Mis Predicciones
+          </CustomLink>
+          <CustomLink to="/scores" isCurrent={isCurrent('scores')}>
+            Tabla de Puntajes
+          </CustomLink>
+          <CustomLink to="/groups" isCurrent={isCurrent('groups')}>
+            Gestionar grupos
+          </CustomLink>
+        </LinkGroup>
+      </LinkbarWrapper>
+    </LinkbarContainer>
   );
 }
