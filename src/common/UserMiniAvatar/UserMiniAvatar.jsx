1d849b0b918faf897057a066118861d53214f10d
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-06-10 19:27:52 -0300

    styles additions

--- src/common/UserMiniAvatar/UserMiniAvatar.jsx
+++ src/common/UserMiniAvatar/UserMiniAvatar.jsx
@@ -1,8 +1,12 @@
 import { HiUserCircle } from 'react-icons/hi';
 
-export function UserMiniAvatar({ name, avatar }) {
+export function UserMiniAvatar({ name, avatar, isSmall }) {
   return avatar ? (
-    <img src={avatar} alt={name || 'user avatar'} />
+    <img
+      src={avatar}
+      alt={name || 'user avatar'}
+      style={isSmall && { width: '20%' }}
+    />
   ) : (
     <HiUserCircle size="2.2rem" />
   );
