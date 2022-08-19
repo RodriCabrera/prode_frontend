d19b96defcb3a1e60fff48f724f8e7d201c9b9e8
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-08-19 20:42:43 -0300

    probando tabs

--- src/pages/Predictions/Banner.jsx
+++ src/pages/Predictions/Banner.jsx
@@ -1,60 +1,59 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { isNil } from 'lodash';
 import { useNavigate } from 'react-router-dom';
 import { Text } from '../../common/common.styles';
 import { checkIfStageIsEnabled } from './predictionsPageUtils';
 import {
-  BannerButton,
-  BannerDataWrapper,
-  BannerTitle,
+	BannerButton,
+	BannerDataWrapper,
+	BannerTitle,
 } from './Predictions.styles';
 
 export function Banner({
-  title,
-  path,
-  percentage,
-  isLoading,
-  editMode,
-  prevStage,
+	title,
+	path,
+	percentage,
+	isLoading,
+	editMode,
+	prevStage,
 }) {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [isDisabled, setIsDisabled] = useState(true);
+	const [isDisabled, setIsDisabled] = useState(true);
 
-  useEffect(() => {
-    checkIfStageIsEnabled(prevStage)
-      .then((res) => setIsDisabled(!res))
-      .catch(() => setIsDisabled(true));
-  }, [editMode, prevStage]);
+	useEffect(() => {
+		checkIfStageIsEnabled(prevStage)
+			.then((res) => setIsDisabled(!res))
+			.catch(() => setIsDisabled(true));
+	}, [editMode, prevStage]);
 
-  const renderPercentageInfo = () => {
-    if (editMode) {
-      if (isLoading) {
-        return <Text weight="200">Cargando %...</Text>;
-      }
-      return (
-        editMode && (
-          <Text weight="200">
-            {!isNil(percentage) ? `${percentage} % completo` : ''}
-          </Text>
-        )
-      );
-    }
-    return null;
-  };
+	const renderPercentageInfo = () => {
+		if (editMode) {
+			if (isLoading) {
+				return <Text weight="200">Cargando %...</Text>;
+			}
+			return (
+				editMode && (
+					<Text weight="200">
+						{!isNil(percentage) ? `${percentage} % completo` : ''}
+					</Text>
+				)
+			);
+		}
+		return null;
+	};
 
-  return (
-    <BannerButton
-      disabled={isDisabled}
-      onClick={() => navigate(path)}
-      tertiary={editMode && !isDisabled}
-      grayscale={isDisabled}
-    >
-      <BannerDataWrapper>
-        <BannerTitle>{title}</BannerTitle>
-        <p>Ver/Editar</p>
-      </BannerDataWrapper>
-      {renderPercentageInfo()}
-    </BannerButton>
-  );
+	return (
+		<BannerButton
+			disabled={isDisabled}
+			onClick={() => navigate(path)}
+			tertiary={editMode && !isDisabled}
+			grayscale={isDisabled}>
+			<BannerDataWrapper>
+				<BannerTitle>{title}</BannerTitle>
+				<p>Ver/Editar</p>
+			</BannerDataWrapper>
+			{renderPercentageInfo()}
+		</BannerButton>
+	);
 }
