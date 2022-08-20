9ca9cce5f18ab16b0644c0338f02323f3c37925b
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-08-20 18:46:16 -0300

    Move FixturePage styles into separate file

--- src/pages/FixturePage/FixturePage.jsx
+++ src/pages/FixturePage/FixturePage.jsx
@@ -6,28 +6,11 @@ import LaterStagesGraph from './components/LaterStagesGraph';
 import { Spinner } from '../../common/Spinner/Spinner';
 import { Text } from '../../common/common.styles';
 import { useFetchFixtureData } from './hooks/useFetchFixtureData';
-
-const FixtureWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
-  min-height: 700px;
-`;
-const GroupTableWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  flex-grow: ${({ fullWidth }) => (fullWidth ? 1 : 'initial')};
-`;
-
-const FixtureTablesContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 2rem 4rem;
-  align-items: center;
-  justify-content: center;
-`;
+import {
+  FixtureTablesContainer,
+  FixtureWrapper,
+  GroupTableWrapper,
+} from './FixturePage.styles';
 
 function Fixture() {
   const { width } = useWindowDimensions();
