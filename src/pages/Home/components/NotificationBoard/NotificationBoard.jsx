28c12d5bedb28649f3f80f55d2a4cc86eea7ee5d
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-08-28 20:39:38 -0300

    changed no prediction notification request

--- src/pages/Home/components/NotificationBoard/NotificationBoard.jsx
+++ src/pages/Home/components/NotificationBoard/NotificationBoard.jsx
@@ -1,25 +1,27 @@
 import { isEmpty } from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { getPredictions } from '../../../../api/predictions';
+import { getPredictionCount } from '../../../../api/predictions';
 import { CardContainer, CardWrapper } from '../../../../common/common.styles';
 import { Spinner } from '../../../../common/Spinner/Spinner';
 import { NoPredictionNotification } from './NoPredictionNotification';
 import useCleanupController from '../../../../hooks/useCleanupController';
 
 function NotificationBoard() {
-  const [predictions, setPredictions] = useState([]);
-  const [loadingCheck, setloadingCheck] = useState({
-    predictions: false,
-    fixture: false,
-  });
+  const [hasPredictions, setHasPredictions] = useState(false);
+  // const [loadingCheck, setloadingCheck] = useState({
+  //   predictions: false,
+  //   fixture: false,
+  // });
+  const [isLoading, setIsLoading] = useState(true)
   const [signal, cleanup, handleCancel] = useCleanupController();
 
   useEffect(() => {
-    getPredictions(undefined, undefined, signal)
-      .then((res) => setPredictions(res.data))
+    setIsLoading(true);
+    getPredictionCount(signal)
+      .then((res) => res.data?.userPredictions > 0 && setHasPredictions(true))
       .catch((err) => handleCancel(err))
       .finally(() => {
-        setloadingCheck({ ...loadingCheck, predictions: true });
+        setIsLoading(false)
       });
     return cleanup;
   }, []);
@@ -29,14 +31,14 @@ function NotificationBoard() {
     // NO GROUP - NO PREDICTIONS:
     // <NoGroupNotification />;
     // YES GROUPS - NO PREDICTIONS:
-    if (loadingCheck.predictions && isEmpty(predictions)) {
+    if (!isLoading && !hasPredictions) {
       return <NoPredictionNotification />;
     }
 
     return <Spinner />;
   }
 
-  if (!isEmpty(predictions) && loadingCheck.predictions) return null;
+  if (hasPredictions && !isLoading) return null;
 
   return (
     <CardContainer>
