80033dbe62f6b11e7612f77245ba127c17da6b35
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-08-29 23:33:42 -0300

    Home page btn styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../common/AuthProvider';
+import { Button } from '../../common/common.styles';
 import LeaderBoard from './components//LeaderBoard/LeaderBoard';
-import Countdown from './components/Countdown';
 import NotificationBoard from './components/NotificationBoard/NotificationBoard';
 import QuickPrediction from './components/QuickPredictions/QuickPrediction';
 import ShortFixture from './components/ShortFixture';
@@ -16,14 +16,23 @@ const PageWrapper = styled.div`
   max-width: 1100px;
   width: 100%;
 `;
-const Row = styled.div`
+const Column = styled.div`
   gap: 2rem;
   flex-wrap: wrap;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Row = styled.div`
+  gap: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
   justify-content: center;
 `;
 
 function Home() {
+  const [openSection, setOpenSection] = useState('fixture');
   const userContext = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -38,12 +47,38 @@ function Home() {
 
   return (
     <PageWrapper>
-      {/* <Countdown /> */}
       <NotificationBoard id="notification-board" />
       <Row>
-        <ShortFixture />
-        <LeaderBoard />
-        <QuickPrediction />
+        <Column>
+          <Button
+            tertiary={openSection === 'fixture'}
+            width="334px"
+            onClick={() => setOpenSection('fixture')}
+          >
+            Próximos partidos
+          </Button>
+          {openSection === 'fixture' && <ShortFixture />}
+        </Column>
+        <Column>
+          <Button
+            tertiary={openSection === 'leaders'}
+            width="334px"
+            onClick={() => setOpenSection('leaders')}
+          >
+            Punteros de grupo
+          </Button>
+          {openSection === 'leaders' && <LeaderBoard />}
+        </Column>
+        <Column>
+          <Button
+            tertiary={openSection === 'quick'}
+            width="334px"
+            onClick={() => setOpenSection('quick')}
+          >
+            Predicción al paso
+          </Button>
+          {openSection === 'quick' && <QuickPrediction />}
+        </Column>
       </Row>
     </PageWrapper>
   );
