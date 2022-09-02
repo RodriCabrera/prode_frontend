217ffe87363eb81567c3c91b379aa21de099d069
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-09-02 12:32:23 -0300

    prepare merge to dev

--- src/router/PredictionsRoutes.jsx
+++ src/router/PredictionsRoutes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PredictionsPage from '../pages/Predictions/PredictionsPage';
 import PredictionManager from '../pages/Predictions/PredictionManager/PredictionManager';
-import NewPredictionManager from '../pages/Predictions/PredictionManager/NewPredictionManager';
 import BannerList from '../pages/Predictions/BannerList/BannerList';
 
 function PredictionsRoutes() {
@@ -10,7 +9,7 @@ function PredictionsRoutes() {
     <Routes>
       <Route path="" element={<PredictionsPage />}>
         <Route index element={<BannerList />} />
-        <Route path="/:phase" element={<NewPredictionManager />} />
+        <Route path="/:phase" element={<PredictionManager />} />
       </Route>
     </Routes>
   );
