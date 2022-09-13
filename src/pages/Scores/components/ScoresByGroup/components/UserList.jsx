03d68943ecb4f12d6f725dfa03e4f6567f94473b
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-09-13 16:58:13 -0300

    add graph detail close button

--- src/pages/Scores/components/ScoresByGroup/components/UserList.jsx
+++ src/pages/Scores/components/ScoresByGroup/components/UserList.jsx
@@ -1,33 +1,69 @@
-import React, { useRef, useEffect } from 'react'
-import { CardContainer, CardWrapper, CardTitle, Text } from '../../../../../common/common.styles'
-import { ListElement } from '../../../../../common/Lists/ListElement'
-import { UserMiniAvatar } from '../../../../../common/UserMiniAvatar/UserMiniAvatar'
+import React, { useRef, useEffect } from 'react';
+import styled from '@emotion/styled';
+import {
+  CardContainer,
+  CardWrapper,
+  CardTitle,
+  Text,
+} from '../../../../../common/common.styles';
+import { ListElement } from '../../../../../common/Lists/ListElement';
+import { UserMiniAvatar } from '../../../../../common/UserMiniAvatar/UserMiniAvatar';
+import { MdOutlineChevronLeft } from 'react-icons/md';
+
+const CloseButton = styled.button`
+  align-self: flex-start;
+  border: none;
+  background: none;
+  text-align: center;
+  cursor: pointer;
+  padding: 0 1rem 0 0;
+  margin: 0;
+`;
 
 export default function UserList({ users, displayInfo, handleClose }) {
-    const listRef = useRef();
+  const listRef = useRef();
 
-    useEffect(() => {
-      const handleClickOutside = (e) => {
-        if (listRef.current && !listRef.current.contains(e.target)) {
-          handleClose();
-        }
-      };
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }, [listRef]);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (listRef.current && !listRef.current.contains(e.target)) {
+        handleClose();
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [listRef]);
   return (
     <CardContainer ref={listRef}>
-        <CardWrapper>
-            <CardTitle>{displayInfo.stage}</CardTitle>
-            <Text align="center">Resultado: <span style={{color: displayInfo.color}}>{displayInfo.name}</span></Text>
-            {users.map(user => {
-                return <ListElement key={user._id} avatar={<UserMiniAvatar avatar={user.avatar} name={user.name} />}>
-                    <Text>{user.name}</Text>
-                </ListElement>
-            })}
-        </CardWrapper>
+      <CardWrapper>
+        <CloseButton onClick={handleClose}>
+          <MdOutlineChevronLeft size={24} />
+        </CloseButton>
+        <CardTitle>{displayInfo.stage}</CardTitle>
+        <Text align="center">
+          Resultado:{' '}
+          <span style={{ color: displayInfo.color }}>{displayInfo.name}</span>
+        </Text>
+        {users.length > 0 ? (
+          users.map((user) => {
+            return (
+              <ListElement
+                key={user._id}
+                avatar={
+                  <UserMiniAvatar avatar={user.avatar} name={user.name} />
+                }
+              >
+                <Text>{user.name}</Text>
+              </ListElement>
+            );
+          })
+        ) : (
+          <Text align="center" margin="auto">
+            Nadie
+          </Text>
+        )}
+      </CardWrapper>
     </CardContainer>
-  )
+  );
 }
