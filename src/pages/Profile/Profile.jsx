7dc42e052bd2b733e6d8b8506d68d2f9a5ed1b8b
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-09-21 16:04:51 -0300

    QAT-86 profile page styles

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -11,6 +11,7 @@ import { ListWrapper } from '../../common/Lists/Lists.styles';
 import { ListElement } from '../../common/Lists/ListElement';
 import { UserMiniAvatar } from '../../common/UserMiniAvatar/UserMiniAvatar';
 import { GoBackButton } from '../../common/GoBackButton/GoBackButton';
+import { useIsMobile } from '../../hooks/useIsMobile';
 import useCleanupController from '../../hooks/useCleanupController';
 
 function Profile() {
@@ -20,6 +21,7 @@ function Profile() {
   const [isLoading, setIsLoading] = useState(false);
   const [groupPredictions, setGroupPredictions] = useState({});
   const [signal, cleanup, handleCancel] = useCleanupController();
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     setIsLoading(true);
@@ -45,15 +47,15 @@ function Profile() {
     <Spinner />
   ) : (
     <CardContainer>
-      <CardWrapper>
-        <GoBackButton />
+      <CardWrapper border={isMobile ? 'none' : null}>
+        <GoBackButton collapse={isMobile}/>
         <UserNameContainer>
           <Text size="1.5rem" weight="bold">
             {profile?.name}
           </Text>
           <UserMiniAvatar name={profile?.name} avatar={profile?.avatar} />
         </UserNameContainer>
-        <Text>Predicciones de {profile.name}:</Text>
+        <Text margin="1.2rem 0 0 0">Predicciones de {profile.name}:</Text>
         <ListWrapper>
           {sharedGroups.map((group) => (
             <ListElement
--- src/pages/Profile/ProfileEdit/ProfileEdit.jsx
+++ src/pages/Profile/ProfileEdit/ProfileEdit.jsx
@@ -8,14 +8,16 @@ import {
   CardWrapper,
   Text,
 } from '../../../common/common.styles';
+import { useIsMobile } from '../../../hooks/useIsMobile';
 import { AuthContext } from '../../../common/AuthProvider';
 
 function ProfileEdit() {
   const userContext = useContext(AuthContext);
+  const isMobile = useIsMobile()
 
   return (
     <CardContainer>
-      <CardWrapper>
+      <CardWrapper border={isMobile ? 'none' : null}>
         {userContext.isLoading ? (
           <Spinner />
         ) : (
