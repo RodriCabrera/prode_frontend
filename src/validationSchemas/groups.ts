119527e1c4b3e06dafc9993349c5478111b12a3d
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-10-28 17:06:09 -0300

    group name validation

--- src/validationSchemas/groups.ts
+++ src/validationSchemas/groups.ts
@@ -1,9 +1,12 @@
 import { object, string, number } from "yup";
 
+const containsBadChars = (string: string | undefined) => string && /[/"?&$:'#%{}();,+@]/.test(string);
+
 const fields = {
   name: string()
     .max(20, "Nombre de máximo 20 caracteres")
     .matches(/[a-zA-Z0-9]/, "El nombre debe tener al menos una letra o número")
+    .test("El nombre no debe contener caracteres especiales", "El nombre no debe contener caracteres especiales", (value) => !containsBadChars(value))
     .required("Completar nombre"),
   manifesto: string()
     .max(500, "Máximo 500 caracteres")
