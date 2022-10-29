2e349258fdc0ccd021d31cbfec0c2bbe760c28c0
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-10-28 22:16:05 -0300

    CI errors comply v2

--- src/pages/GroupPage/components/GroupRules.jsx
+++ src/pages/GroupPage/components/GroupRules.jsx
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Text, TextGroup } from '../../../common/common.styles';
 import { References } from '../../../common/References';
 import { translateDuration } from '../../pagesHelpers';
-=======
-import React from "react";
-import { Text } from "../../../common/common.styles";
-import { References } from "../../../common/References";
-import { translateDuration } from "../../pagesHelpers";
->>>>>>> a4568261c9c39d5bc652e02819e8ff5c29600dce
 
 function GroupRules({ rules }) {
   if (!rules) return null;
@@ -23,11 +16,7 @@ function GroupRules({ rules }) {
           <br />
         </>
       )}
-<<<<<<< HEAD
       <TextGroup>
-=======
-      <div style={{ display: "flex", gap: "1rem", marginBottom: "2rem" }}>
->>>>>>> a4568261c9c39d5bc652e02819e8ff5c29600dce
         <Text size="1.2rem" weight="600" withBottomBorder>
           Tiempos límite:
         </Text>
--- src/pages/Groups/components/GroupConfirm.jsx
+++ src/pages/Groups/components/GroupConfirm.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Text, CardTitle, Button } from '../../../common/common.styles';
 import { translateDuration } from '../../pagesHelpers';
@@ -51,55 +50,3 @@ function GroupConfirm({ groupName, userGroupData, confirmText }) {
 }
 
 export default GroupConfirm;
-=======
-import React from "react";
-import { Text, CardTitle, Button } from "../../../common/common.styles";
-import { translateDuration } from "../../pagesHelpers";
-
-function GroupConfirm({ groupName, userGroupData, confirmText }) {
-  return (
-    userGroupData && (
-      <>
-        <CardTitle>{groupName}</CardTitle>
-        {userGroupData.manifesto && (
-          <>
-            <Text size="1.2rem" withBottomBorder>
-              Reglas
-            </Text>
-            <Text>{userGroupData.manifesto}</Text>
-            <br />
-          </>
-        )}
-        <Text size="1.2rem" withBottomBorder>
-          Sistema de puntaje
-        </Text>
-        <div>
-          <Text align="center" color="lightgreen">
-            Resultado exacto: {userGroupData.scoring.FULL}
-          </Text>
-          <br />
-          <Text align="center" color="yellow">
-            Ganador: {userGroupData.scoring.WINNER}
-          </Text>
-          <br />
-          <Text align="center" color="red">
-            Resultado erróneo: {userGroupData.scoring.NONE}
-          </Text>
-        </div>
-        <br />
-        <Text size="1.2rem" withBottomBorder>
-          Tiempos
-        </Text>
-        <Text>
-          Puedes realizar predicciones hasta
-          {translateDuration(userGroupData.timeLimit)}
-        </Text>
-        <br />
-        <Button type="submit">{confirmText}</Button>
-      </>
-    )
-  );
-}
-
-export default GroupConfirm;
->>>>>>> a4568261c9c39d5bc652e02819e8ff5c29600dce
