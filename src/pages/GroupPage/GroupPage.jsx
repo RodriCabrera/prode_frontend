b381c80ba2655c4150dba99dfd10fa5b598954ec
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-11-01 16:53:40 -0300

    remove warning unused import

--- src/pages/GroupPage/GroupPage.jsx
+++ src/pages/GroupPage/GroupPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, createContext } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { getGroupData } from "../../api/groups";
