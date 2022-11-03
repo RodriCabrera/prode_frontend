571e89f054da38b8d0f03a080a7d5c41bb0a77e2
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-11-02 22:25:57 -0300

    set fixed video for landing page background

--- src/api/pexels.js
+++ src/api/pexels.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+// function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
 
 export const getSoccerVideos = () => {
-  let page = getRandomArbitrary(1, 20);
+  // let page = getRandomArbitrary(1, 20);
   return axios.get(
-    `https://api.pexels.com/videos/search?query=soccer&page=${page}`,
+    `https://api.pexels.com/videos/videos/6079615`,
     // eslint-disable-next-line no-undef
     { headers: { Authorization: process.env.REACT_APP_PEXELS_API_KEY } }
   );
