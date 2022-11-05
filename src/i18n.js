00bd85af8b4a267f99ace93bdaad306bd8380c66
Rodrigo <rodrigo1cabrera2@gmail.com>
2022-11-04 21:54:05 -0300

    set spanish as initial lang

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { enTranslation } from "./langs/en";
 import { esTranslation } from "./langs/es";
 
@@ -8,12 +7,13 @@ i18n
 
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    lng: "es",
     debug: true,
     fallbackLng: "es",
     interpolation: {
